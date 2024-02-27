62b7eab52d99ea0983f02b366a5c54f0e93d11b7
braczkot <112940576+braczkot@users.noreply.github.com>
2024-02-27 13:40:42 +0100

    Add files via upload

---
+++ WPF/Adatkarbantartas/LoginScreen.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Adatkarbantartas
+{
+    /// <summary>
+    /// Interaction logic for LoginScreen.xaml
+    /// </summary>
+    public partial class LoginScreen : Window
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            if(txtEmail.Text == "admin123@gmail.com" && txtPassword.Password == "password")
+            {
+                MainWindow objWindow = new MainWindow();
+                objWindow.Show();
+                this.Close();
+            }
+            else if(txtEmail.Text == "" || txtPassword.Password == "" || (txtEmail.Text == "" && txtPassword.Password == "")) 
+            {
+                MessageBox.Show("Missing login credentials!");
+            }
+            else
+            {
+                MessageBox.Show("Wrong E-mail, and/or password!");
+            }
+            
+        }
+    }
+}
