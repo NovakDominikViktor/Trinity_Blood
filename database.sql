46153bed1ca3c3e8f01e9d4d2f83806bfc9aaea4
NovakDominikViktor <112945006+NovakDominikViktor@users.noreply.github.com>
2024-03-18 20:29:53 +0100

    Now the time when the products are posted are also there

--- database.sql
+++ database.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2024. Már 14. 10:36
--- Kiszolgáló verziója: 10.4.32-MariaDB
--- PHP verzió: 8.0.30
+-- Létrehozás ideje: 2024. Már 18. 20:29
+-- Kiszolgáló verziója: 10.4.28-MariaDB
+-- PHP verzió: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -101,7 +101,8 @@ CREATE TABLE `aspnetuserroles` (
 
 INSERT INTO `aspnetuserroles` (`UserId`, `RoleId`) VALUES
 ('2c6fc608-71ae-473f-a6e0-5d09d75deab5', '780067f7-8b03-4131-9b00-f9f05892fa43'),
-('2c6fc608-71ae-473f-a6e0-5d09d75deab5', '929fd86e-d0ae-4f00-937e-3e77f710227f');
+('2c6fc608-71ae-473f-a6e0-5d09d75deab5', '929fd86e-d0ae-4f00-937e-3e77f710227f'),
+('2fa5b17c-1990-403f-8fb3-cf4e2d90a397', '929fd86e-d0ae-4f00-937e-3e77f710227f');
 
 -- --------------------------------------------------------
 
@@ -135,7 +136,8 @@ CREATE TABLE `aspnetusers` (
 --
 
 INSERT INTO `aspnetusers` (`Id`, `UserName`, `NormalizedUserName`, `Email`, `NormalizedEmail`, `EmailConfirmed`, `PasswordHash`, `SecurityStamp`, `ConcurrencyStamp`, `PhoneNumber`, `PhoneNumberConfirmed`, `TwoFactorEnabled`, `LockoutEnd`, `LockoutEnabled`, `AccessFailedCount`, `FullName`, `FirstName`, `LastName`) VALUES
-('2c6fc608-71ae-473f-a6e0-5d09d75deab5', 'admin@admin.com', 'ADMIN@ADMIN.COM', 'admin@admin.com', 'ADMIN@ADMIN.COM', 0, 'AQAAAAIAAYagAAAAEBtJdS1IWGiAk13nvzWMpH5HU57iQzR3NUVocHGRrrHq2F8iT5EcBpyoqpeHaZuz3Q==', 'MWMP5LFPHIVO575O2UNCNK2452VPQWOW', 'f5433e7f-6e67-4a1a-84c4-9076544701c1', NULL, 0, 0, NULL, 1, 0, 'Admin Admin', 'Admin', 'Admin');
+('2c6fc608-71ae-473f-a6e0-5d09d75deab5', 'admin@admin.com', 'ADMIN@ADMIN.COM', 'admin@admin.com', 'ADMIN@ADMIN.COM', 0, 'AQAAAAIAAYagAAAAEBtJdS1IWGiAk13nvzWMpH5HU57iQzR3NUVocHGRrrHq2F8iT5EcBpyoqpeHaZuz3Q==', 'MWMP5LFPHIVO575O2UNCNK2452VPQWOW', 'f5433e7f-6e67-4a1a-84c4-9076544701c1', NULL, 0, 0, NULL, 1, 0, 'Admin Admin', 'Admin', 'Admin'),
+('2fa5b17c-1990-403f-8fb3-cf4e2d90a397', 'novakd@kkszki.hu', 'NOVAKD@KKSZKI.HU', 'novakd@kkszki.hu', 'NOVAKD@KKSZKI.HU', 0, 'AQAAAAIAAYagAAAAEDhEEwR2oweGheIUYXsPcvAF+GUSP9DqZxk1dmjgenzYHCfXEQ3aL4gTZn2RA4e6dQ==', 'XD3C7RLUMBYCCL2TOGCSR6VCMC2HLF24', 'c6f9cdb8-dc51-40f6-a490-8e1cf7baba06', NULL, 0, 0, NULL, 1, 0, 'Dominik Novák', 'Dominik', 'Novák');
 
 --
 -- Eseményindítók `aspnetusers`
@@ -197,6 +199,13 @@ CREATE TABLE `comments` (
   `ReviewDate` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- A tábla adatainak kiíratása `comments`
+--
+
+INSERT INTO `comments` (`Id`, `UserId`, `ProductId`, `Ratings`, `Comments`, `ReviewDate`) VALUES
+(18, '2fa5b17c-1990-403f-8fb3-cf4e2d90a397', 4, 3.5, 'this is a nice shampoo', '2024-03-18 18:47:16');
+
 -- --------------------------------------------------------
 
 --
@@ -231,34 +240,35 @@ CREATE TABLE `products` (
   `IsItInStock` tinyint(1) NOT NULL,
   `CategoryId` int(11) NOT NULL,
   `PictureUrl` longtext NOT NULL,
-  `StorageStock` int(11) NOT NULL
+  `StorageStock` int(11) NOT NULL,
+  `PostedTime` datetime(6) NOT NULL DEFAULT '2020-03-14 01:01:01.000000'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- A tábla adatainak kiíratása `products`
 --
 
-INSERT INTO `products` (`Id`, `Name`, `Description`, `Price`, `IsItInStock`, `CategoryId`, `PictureUrl`, `StorageStock`) VALUES
-(1, 'Aspirin Ultra', 'Pain reliever and fever reducer', 5.99, 1, 1, 'https://pingvinpatika.hu/pub/media/catalog/product/a/s/aspirin-ultra-500mg-bevont-tabletta-8x-296148-2.jpg', 100),
-(2, 'Ibuprofen', 'Pain reliever and anti-inflammatory', 7.49, 1, 1, 'https://s.benu.hu/media/catalog/product/cache/1b657306323fd5cf9ea727d8a6c2df8f/i/b/ibuprofen_polfa_200_mg_bevont_tabletta_100x_147876_2016.jpg', 150),
-(3, 'Vitamin C', 'Immune system support', 9.99, 1, 3, 'https://s.benu.hu/media/catalog/product/cache/1b657306323fd5cf9ea727d8a6c2df8f/e/g/egis_cvitamin_500mg.jpg', 200),
-(4, 'L’Oréal Paris Elseve Hyaluron Plump Shampoo', 'Cleanses and nourishes hair', 12.99, 1, 4, 'https://licilasicdn.s3.amazonaws.com/public/product_images/43670/main/original.jpg', 80),
-(5, 'Oral-B Complete Plus Protect & Clean Toothpaste Fresh Mint 75ml', 'Fights cavities and freshens breath', 3.49, 1, 4, 'https://static.beautytocare.com/media/catalog/product/o/r/oral-b-complete-plus-mouthwash-toothpaste-75ml_1.jpg', 120),
-(6, 'La PIEL Hand Cream', 'Moisturizes and softens hands', 6.99, 1, 2, 'https://licilasicdn.s3.amazonaws.com/public/product_images/41502/main/original.jpg', 90),
-(7, 'Garnier Skin Natural Hyaluronic Aloe Face Mask', 'Cleanses pores and revitalizes skin', 4.99, 1, 2, 'https://licilasicdn.s3.amazonaws.com/public/product_images/39458/main/original.jpg', 70),
-(8, 'Nature\'s Bounty® Fish Oil', 'Supports heart and joint health', 15.99, 1, 3, 'https://naturesbounty.com/cdn/shop/products/087800.png?v=1667506722', 100),
-(9, 'Labello Soft Rosé Lip Balm 4.8g', 'Hydrates and protects lips', 2.99, 1, 4, 'https://static.beautytocare.com/media/catalog/product/l/a/labello-soft-rose-lip-balm-4-8g.jpg', 110),
-(10, 'Pyunkang Yul - ACNE Cream 50ml', 'Treats and prevents acne', 8.49, 1, 2, 'https://cosibella.hu/hun_pl_Pyunkang-Yul-ACNE-Cream-Akne-elleni-arckrem-50ml-1219_1.webp', 60),
-(11, 'Professional Style Pink Hair Gel Strong With Pro Vitamin B5', 'Provides strong hold and shine', 6.49, 1, 4, 'https://i.makeup.hu/n/ng/ngd5of9gjmoe.jpg', 70),
-(12, 'Centrum Advance Multivitamin Multimineral Tablets', 'Supports overall health and wellness', 11.99, 1, 3, 'https://media.glamourmagazine.co.uk/photos/63da7c23175377e5c1b41fee/1:1/w_1280%2Cc_limit/MULTIVITAMINS%2520010223%25202.jpg', 180),
-(13, 'Coppertone SPORT Sunscreen SPF 50 Lotion, Water Resistant Sunscreen, Body Sunscreen Lotion, 7 Fl Oz', 'Protects against UV rays', 14.99, 1, 2, 'https://m.media-amazon.com/images/I/71I+WE0s1gL.jpg', 100),
-(14, 'Equate Ultra Strength Antacid Tropical Fruit Chewable Tablets, 1000 mg, 160 Count', 'Relieves heartburn and acid indigestion', 4.49, 1, 1, 'https://i5.walmartimages.com/seo/Equate-Ultra-Strength-Antacid-Tropical-Fruit-Chewable-Tablets-1000-mg-160-Count_9f61df61-673e-4919-bfb2-03e84e028b90.c8effce29e8c1ae23262a32055eeb3be.jpeg', 120),
-(15, 'Dove Original Stick Anti-Perspirant Deodorant 40ml', 'Provides long-lasting odor protection', 5.99, 1, 4, 'https://m.media-amazon.com/images/I/61HJyR+i8LL.jpg', 100),
-(16, 'Handy Pocket Pal Redness Relief Eye Drops, Travel Size', 'Relieves dry and irritated eyes', 7.99, 1, 1, 'https://pics.walgreens.com/prodimg/597605/450.jpg', 80),
-(17, 'Dermatologist Moisturizing Body Lotion 6% Urea', 'Hydrates and soothes dry skin', 10.99, 1, 2, 'https://skinsmart.hu/media/catalog/product/cache/8ac1e0cc7038d8ae53e7e71f09f92dfd/6/u/6urea-testapolo.jpg', 150),
-(18, 'Hydrolyzed Whey Protein Powder XS™ Cocoa - Chocolate\r\n', 'Supports muscle growth and recovery', 19.99, 1, 3, 'https://media.amway.eu/sys-master/images/h0e/h55/11562589618206/product-image_800_800_121606_new.jpg_30d89cbf-fbab-435c-8d04-ba6155688b97_rcv_IMAGE_product-image_600_600', 90),
-(19, 'Savage for Men 3.4 Oz Men\'s Eau De Toilette Spray Refreshing & Warm Masculine Scent for Daily Use Men\'s Casual Cologne Includes NovoGlow Carrying Pouch Smell Fresh All Day A Gift for Any Occasion', 'Provides a refreshing scent', 24.99, 1, 4, 'https://m.media-amazon.com/images/I/91Eqa0ZQCLL._AC_UF1000,1000_QL80_.jpg', 60),
-(20, 'Colgate® Total® Mint Waxed Dental Floss', 'Removes plaque and food particles', 1.99, 1, 4, 'https://www.colgate.com.au/content/dam/cp-sites/oral-care/oral-care-center/en-au/product-detail-pages/specialty-products/colgate-total-mint-waxed.jpg', 200);
+INSERT INTO `products` (`Id`, `Name`, `Description`, `Price`, `IsItInStock`, `CategoryId`, `PictureUrl`, `StorageStock`, `PostedTime`) VALUES
+(1, 'Aspirin Ultra', 'Pain reliever and fever reducer', 5.99, 1, 1, 'https://pingvinpatika.hu/pub/media/catalog/product/a/s/aspirin-ultra-500mg-bevont-tabletta-8x-296148-2.jpg', 100, '2020-03-14 01:01:01.000000'),
+(2, 'Ibuprofen', 'Pain reliever and anti-inflammatory', 7.49, 1, 1, 'https://s.benu.hu/media/catalog/product/cache/1b657306323fd5cf9ea727d8a6c2df8f/i/b/ibuprofen_polfa_200_mg_bevont_tabletta_100x_147876_2016.jpg', 150, '2020-03-15 01:01:01.000000'),
+(3, 'Vitamin C', 'Immune system support', 9.99, 1, 3, 'https://s.benu.hu/media/catalog/product/cache/1b657306323fd5cf9ea727d8a6c2df8f/e/g/egis_cvitamin_500mg.jpg', 200, '2020-03-14 01:01:01.000000'),
+(4, 'L’Oréal Paris Elseve Hyaluron Plump Shampoo', 'Cleanses and nourishes hair', 12.99, 1, 4, 'https://licilasicdn.s3.amazonaws.com/public/product_images/43670/main/original.jpg', 80, '2020-03-30 01:01:01.000000'),
+(5, 'Oral-B Complete Plus Protect & Clean Toothpaste Fresh Mint 75ml', 'Fights cavities and freshens breath', 3.49, 1, 4, 'https://static.beautytocare.com/media/catalog/product/o/r/oral-b-complete-plus-mouthwash-toothpaste-75ml_1.jpg', 120, '2020-03-14 01:01:01.000000'),
+(6, 'La PIEL Hand Cream', 'Moisturizes and softens hands', 6.99, 1, 2, 'https://licilasicdn.s3.amazonaws.com/public/product_images/41502/main/original.jpg', 90, '2020-03-14 01:01:01.000000'),
+(7, 'Garnier Skin Natural Hyaluronic Aloe Face Mask', 'Cleanses pores and revitalizes skin', 4.99, 1, 2, 'https://licilasicdn.s3.amazonaws.com/public/product_images/39458/main/original.jpg', 70, '2020-03-14 01:01:01.000000'),
+(8, 'Nature\'s Bounty® Fish Oil', 'Supports heart and joint health', 15.99, 1, 3, 'https://naturesbounty.com/cdn/shop/products/087800.png?v=1667506722', 100, '2022-03-01 01:01:01.000000'),
+(9, 'Labello Soft Rosé Lip Balm 4.8g', 'Hydrates and protects lips', 2.99, 1, 4, 'https://static.beautytocare.com/media/catalog/product/l/a/labello-soft-rose-lip-balm-4-8g.jpg', 110, '2020-03-14 01:01:01.000000'),
+(10, 'Pyunkang Yul - ACNE Cream 50ml', 'Treats and prevents acne', 8.49, 1, 2, 'https://cosibella.hu/hun_pl_Pyunkang-Yul-ACNE-Cream-Akne-elleni-arckrem-50ml-1219_1.webp', 60, '2020-03-14 01:01:01.000000'),
+(11, 'Professional Style Pink Hair Gel Strong With Pro Vitamin B5', 'Provides strong hold and shine', 6.49, 1, 4, 'https://i.makeup.hu/n/ng/ngd5of9gjmoe.jpg', 70, '2021-04-24 01:01:01.000000'),
+(12, 'Centrum Advance Multivitamin Multimineral Tablets', 'Supports overall health and wellness', 11.99, 1, 3, 'https://media.glamourmagazine.co.uk/photos/63da7c23175377e5c1b41fee/1:1/w_1280%2Cc_limit/MULTIVITAMINS%2520010223%25202.jpg', 180, '2020-03-14 01:01:01.000000'),
+(13, 'Coppertone SPORT Sunscreen SPF 50 Lotion, Water Resistant Sunscreen, Body Sunscreen Lotion, 7 Fl Oz', 'Protects against UV rays', 14.99, 1, 2, 'https://m.media-amazon.com/images/I/71I+WE0s1gL.jpg', 100, '2020-08-18 01:01:01.000000'),
+(14, 'Equate Ultra Strength Antacid Tropical Fruit Chewable Tablets, 1000 mg, 160 Count', 'Relieves heartburn and acid indigestion', 4.49, 1, 1, 'https://i5.walmartimages.com/seo/Equate-Ultra-Strength-Antacid-Tropical-Fruit-Chewable-Tablets-1000-mg-160-Count_9f61df61-673e-4919-bfb2-03e84e028b90.c8effce29e8c1ae23262a32055eeb3be.jpeg', 120, '2020-03-14 01:01:01.000000'),
+(15, 'Dove Original Stick Anti-Perspirant Deodorant 40ml', 'Provides long-lasting odor protection', 5.99, 1, 4, 'https://m.media-amazon.com/images/I/61HJyR+i8LL.jpg', 100, '2020-03-14 01:01:01.000000'),
+(16, 'Handy Pocket Pal Redness Relief Eye Drops, Travel Size', 'Relieves dry and irritated eyes', 7.99, 1, 1, 'https://pics.walgreens.com/prodimg/597605/450.jpg', 80, '2020-03-14 01:01:01.000000'),
+(17, 'Dermatologist Moisturizing Body Lotion 6% Urea', 'Hydrates and soothes dry skin', 10.99, 1, 2, 'https://skinsmart.hu/media/catalog/product/cache/8ac1e0cc7038d8ae53e7e71f09f92dfd/6/u/6urea-testapolo.jpg', 150, '2020-03-14 01:01:01.000000'),
+(18, 'Hydrolyzed Whey Protein Powder XS™ Cocoa - Chocolate\r\n', 'Supports muscle growth and recovery', 19.99, 1, 3, 'https://media.amway.eu/sys-master/images/h0e/h55/11562589618206/product-image_800_800_121606_new.jpg_30d89cbf-fbab-435c-8d04-ba6155688b97_rcv_IMAGE_product-image_600_600', 90, '2020-03-14 01:01:01.000000'),
+(19, 'Savage for Men 3.4 Oz Men\'s Eau De Toilette Spray Refreshing & Warm Masculine Scent for Daily Use Men\'s Casual Cologne Includes NovoGlow Carrying Pouch Smell Fresh All Day A Gift for Any Occasion', 'Provides a refreshing scent', 24.99, 1, 4, 'https://m.media-amazon.com/images/I/91Eqa0ZQCLL._AC_UF1000,1000_QL80_.jpg', 60, '2024-03-01 01:01:01.000000'),
+(20, 'Colgate® Total® Mint Waxed Dental Floss', 'Removes plaque and food particles', 1.99, 1, 4, 'https://www.colgate.com.au/content/dam/cp-sites/oral-care/oral-care-center/en-au/product-detail-pages/specialty-products/colgate-total-mint-waxed.jpg', 200, '2020-03-14 01:01:01.000000');
 
 --
 -- Eseményindítók `products`
@@ -298,7 +308,8 @@ INSERT INTO `__efmigrationshistory` (`MigrationId`, `ProductVersion`) VALUES
 ('20240313071854_DecimalToDouble', '8.0.1'),
 ('20240313074008_StrogeToStorage', '8.0.1'),
 ('20240314072051_ConnectOrderToProductAndUser', '8.0.1'),
-('20240314073832_CommentRemovedFromComment', '8.0.1');
+('20240314073832_CommentRemovedFromComment', '8.0.1'),
+('20240317151155_addedTimeToProducts', '8.0.1');
 
 --
 -- Indexek a kiírt táblákhoz
@@ -414,7 +425,7 @@ ALTER TABLE `categories`
 -- AUTO_INCREMENT a táblához `comments`
 --
 ALTER TABLE `comments`
-  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT a táblához `orders`
